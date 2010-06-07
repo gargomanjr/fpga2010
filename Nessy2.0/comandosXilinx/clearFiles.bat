808d08c298651a84d2447f2ea5d2edbc12a9b170
lacamaro@hotmail.com <lacamaro@hotmail.com@493b663f-1ba6-cc75-0679-7ade71339bd9>
2010-06-07 12:02:41 +0000

    Actualizada memoria. ensayo.ucf ahora se llama constraints.ucf Modificados los comandos que lo referenciaban

--- Nessy2.0/comandosXilinx/clearFiles.bat
+++ Nessy2.0/comandosXilinx/clearFiles.bat
@@ -1,7 +1,7 @@
 md %temp%\tempvhdlprj
 copy ..\IOSerie\Circuito_FPGA.prj %temp%\tempvhdlprj
 copy ..\IOSerie\*.vhd %temp%\tempvhdlprj
-copy ..\IOSerie\ensayo.ucf %temp%\tempvhdlprj
+copy ..\IOSerie\constraints.ucf %temp%\tempvhdlprj
 del /Q ..\IOSerie\.
 copy %temp%\tempvhdlprj\ ..\IOSerie 
 del /Q %temp%\tempvhdlprj\. 
--- Nessy2.0/comandosXilinx/translateNGDBUILD.bat
+++ Nessy2.0/comandosXilinx/translateNGDBUILD.bat
@@ -6,4 +6,4 @@ cd C:\Xilinx10.1\ISE\lib\nt
 
 set XILINX=C:\Xilinx10.1\ISE\
 
-C:\Xilinx10.1\ISE\bin\nt\unwrapped\ngdbuild.exe -uc %ioserie%\ensayo.ucf -intstyle xflow -dd _ngo -nt timestamp -p xc2vp30-ff896-7 %ioserie%\Circuito_FPGA.ngc %ioserie%\Circuito_FPGA.ngd
+C:\Xilinx10.1\ISE\bin\nt\unwrapped\ngdbuild.exe -uc %ioserie%\constraints.ucf -intstyle xflow -dd _ngo -nt timestamp -p xc2vp30-ff896-7 %ioserie%\Circuito_FPGA.ngc %ioserie%\Circuito_FPGA.ngd
